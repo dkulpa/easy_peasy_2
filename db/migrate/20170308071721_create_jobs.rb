6406da4257da099c8d6a10288bb44fb5fbb5ad16
dkulpa <d.kulpa@selleo.com>
2017-03-08 08:17:44 +0100

    create model Job

---
+++ db/migrate/20170308071721_create_jobs.rb
@@ -0,0 +1,11 @@
+class CreateJobs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jobs do |t|
+      t.string :name
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308071356) do
+ActiveRecord::Schema.define(version: 20170308071721) do
 
   create_table "availabilities", force: :cascade do |t|
     t.integer  "user_id"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170308071356) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_availabilities_on_user_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
