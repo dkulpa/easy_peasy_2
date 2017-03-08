fe4de914d323e115c7e116b9d31d9fc28f5391d4
dkulpa <d.kulpa@selleo.com>
2017-03-08 09:45:50 +0100

    seeds refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,38 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Job.create(start_date: DateTime.new(2017, 1, 2), end_date: DateTime.new(2017, 1, 4))
-User.create(first_name: 'Greg', last_name: 'Cubrick', email: 'greg.cubrick@example.com')
-
-unaccepted_profile = Profile.create(phone_number: '873229123')
-User.create(first_name: 'Amy',
-  last_name: 'Sadstring',
-  email: 'amy.sadstring@example.com',
-  profile: unaccepted_profile)
-
-accepted_profile = Profile.create(phone_number: '726290830', accepted_at: DateTime.new(2017, 1, 2))
-User.create(first_name: 'Goddie',
-  last_name: 'Poodie',
-  email: 'goodie.poodie@example.com',
-  profile: accepted_profile,
-  availabilities: [Availability.create])
-
-User.create(first_name: 'John',
-  last_name: 'Cobalt',
-  email: 'john.cobalt@example.com',
-  profile: accepted_profile,
-  availabilities: [
-    Availability.create(day_off: DateTime.new(2017, 1, 1)),
-    Availability.create(day_off: DateTime.new(2017, 1, 5))
-  ]
+Job.create(
+  start_date: DateTime.new(2017, 1, 2),
+  end_date: DateTime.new(2017, 1, 4)
 )
 
-User.create(first_name: 'Mara',
-  last_name: 'Splitkova',
-  email: 'mara.splitkova@example.com',
-  profile: accepted_profile,
-  availabilities: [
-    Availability.create(day_off: DateTime.new(2017, 1, 2)),
-    Availability.create(day_off: DateTime.new(2017, 1, 10))
-  ]
-)
+profiles = Profile.create([
+  { phone_number: '873229123' },
+  { phone_number: '726290830', accepted_at: DateTime.new(2017, 1, 2) },
+  { phone_number: '234991092', accepted_at: DateTime.new(2017, 1, 3) },
+  { phone_number: '362891031', accepted_at: DateTime.new(2017, 1, 4) }
+])
+
+availabilities = Availability.create([
+  { day_off: DateTime.new(2017, 1, 1) },
+  { day_off: DateTime.new(2017, 1, 5) },
+  { day_off: DateTime.new(2017, 1, 2) },
+  { day_off: DateTime.new(2017, 1, 10) }
+])
+
+User.create([
+  { first_name: 'Greg', last_name: 'Cubrick', email: 'greg.cubrick@example.com' },
+  { first_name: 'Amy',
+    last_name:  'Sadstring',
+    email:      'amy.sadstring@example.com',
+    profile:    profiles[0]
+  },
+  { first_name:     'Goddie',
+    last_name:      'Poodie',
+    email:          'goodie.poodie@example.com',
+    profile:        profiles[1],
+    availabilities: [Availability.create]
+  },
+  { first_name:     'John',
+    last_name:      'Cobalt',
+    email:          'john.cobalt@example.com',
+    profile:        profiles[2],
+    availabilities: [
+      availabilities[0],
+      availabilities[1]
+    ]
+  },
+  { first_name:     'Mara',
+    last_name:      'Splitkova',
+    email:          'mara.splitkova@example.com',
+    profile:        profiles[3],
+    availabilities: [
+      availabilities[2],
+      availabilities[3]
+    ]
+  }
+])
