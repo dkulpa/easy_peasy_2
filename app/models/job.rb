3d9dfd6904662fbf96d63567fbbcef844995e8fa
dkulpa <d.kulpa@selleo.com>
2017-03-08 11:36:03 +0100

    add find_available_users method to Job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,11 @@
 class Job < ApplicationRecord
+  def find_available_users
+    User.joins(:profile).where(
+      '(phone_number AND accepted_at) NOT NULL'
+    ).joins(:availabilities).where(
+      'day_off NOT BETWEEN ? AND ?',
+      start_date,
+      end_date
+    )
+  end
 end
